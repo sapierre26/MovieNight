53d0a6e996af8e16a4b1be609e601c55713f1e92
Sanaia <sgpierre2003@gmail.com>
2025-10-29 18:40:32 -0700

    almost done with Lab 8

--- packages/proto/src/soundtrack-library-grid-item.ts
+++ packages/proto/src/soundtrack-library-grid-item.ts
@@ -1,20 +1,105 @@
 import { html, css, LitElement } from "lit";
 import { property } from "lit/decorators.js";
+import reset from "./styles/reset.css.ts";
 
 export class SoundtrackLibraryGridItemElement extends LitElement {
+    @property({ attribute: "img-src" })
+    imgSrc?: string;
+
+    @property({ attribute: "soundtrack-name" })
+    soundtrackName?: string;
+
+    @property()
+    runtime?: string;
+
+    @property()
+    href?: string;
+
     override render() {
         return html `
             <article class="soundtrack-library-grid-item">
-                <article class="soundtrack-library-grid-item-img"><img src="movie-flyer-images-horizontal/star-wars.png" alt="Star Wars"></article>
+                <article class="soundtrack-library-grid-item-img">
+                    <slot name="movie-flyer"><img src="${this.imgSrc} alt="${this.soundtrackName}"></slot>
+                </article>
 
                 <article class="soundtrack-library-grid-item-text">
-                    <p>Star Wars: A New Hope (Original Motion Pictures)</p>
-                    <p id="runtime">1 hr 15 min</p>
-                    <a href="soundtrack-item.html"><button>Listen to Full Playlist</button></a>
+                    <p><slot name="movie-name">${this.soundtrackName}</slot></p>
+                    <p id="runtime"><slot name="duration">${this.runtime}</slot></p>
+                    <a href="${this.href}"><button><slot name="soundtrack-songs">Listen to Full Playlist</slot></button></a>
                 </article>
             </article>
         `;
     }
 
-    static styles = css``;
+    static styles = [
+        reset.styles,
+        css`
+        .soundtrack-library-grid-item {
+            display: flex;
+            padding: var(--padding-insider);
+            flex: 1;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 250px;
+            width: 250px;
+            height: auto;
+            background-color: var(--color-main-background);
+            border: var(--border-thickness-content) solid var(--color-main-support);
+            border-radius: var(--border-radius-content);
+        }
+
+        .soundtrack-library-grid-item-text {
+            width: 200px;
+        }
+
+        #runtime {
+            color: var(--color-main-support);
+            font-family: var(--sub-font-family);
+            font-weight: var(--main-font-weight);
+            font-style: var(--main-font-type);
+            font-size: var(--p-font-size-smaller);
+        }
+
+        .soundtrack-library-grid-item-text button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: var(--color-sub-background);
+            color: var(--color-button-text);
+            text-align: center;
+            text-decoration: none;
+            width: 200px;
+            height: 30px;
+            font-family: var(--sub-font-family);
+            font-weight: var(--main-font-weight);
+            font-style: var(--main-font-type);
+            font-size: var(--p-font-size);
+            border: var(--border-thickness-content) solid var(--color-sub-background);
+            border-radius: var(--border-radius-content);
+            cursor: pointer;
+            transition: transform 0.3s ease;
+        }
+
+        .soundtrack-library-grid-item-text button:hover {
+            background-color: var(--color-button-text);
+            color: var(--color-sub-background);
+            text-decoration: underline;
+            transform: scale(1.03);
+        }
+
+        .soundtrack-library-grid-item-img img {
+            width: 200px;
+            height: 191px;
+        }
+
+        .soundtrack-library-grid-item-text p {
+            color: var(--color-main-support);
+            font-family: var(--sub-font-family);
+            font-weight: var(--main-font-weight);
+            font-style: var(--main-font-type);
+            font-size: var(--h3-font-size);
+        }
+    `
+    ];
 }
---
+++ packages/proto/vite.config.js
@@ -0,0 +1,25 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        // sub: resolve(__dirname, 'movies-out-now.html'),
+        // sub: resolve(__dirname, 'movies-out-now-item.html'),
+        // sub: resolve(__dirname, 'movie-library.html'),
+        // sub: resolve(__dirname, 'movie-library-item.html'),
+        // sub: resolve(__dirname, 'theaters.html'),
+        // sub: resolve(__dirname, 'theaters-item.html'),
+        sub: resolve(__dirname, "soundtrack-library.html"),
+        // sub: resolve(__dirname, 'soundtrack-library-item.html'),
+        // sub: resolve(__dirname, 'artifacts-library.html'),
+        // sub: resolve(__dirname, 'film-locations.html')
+      },
+    },
+  },
+});
