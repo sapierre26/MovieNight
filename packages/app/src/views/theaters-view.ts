deb988cacf62a8ce74a94051468c07f9a9af790b
Sanaia <sgpierre2003@gmail.com>
2025-11-19 13:21:02 -0800

    still working on Lab 13

--- packages/app/src/views/theaters-view.ts
+++ packages/app/src/views/theaters-view.ts
@@ -1,9 +1,9 @@
 import { html, LitElement } from "lit";
 
 export class TheatersViewElement extends LitElement {
-    render() {
-        return html`
-            <theaters-list src="/functions/theaters-data.json"></theaters-list>
-        `
-    }
+  render() {
+    return html`
+      <theaters-list src="/functions/theaters-data.json"></theaters-list>
+    `;
+  }
 }
